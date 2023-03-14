59e5ebc58878b1f9558aed3b821a49dcc174659e
Your Name <mohamedanwerali8@gmail.com>
2023-03-14 06:49:10 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FacebookApp(),
+    );
+  }
+}
+
+class FacebookApp extends StatelessWidget {
+  const FacebookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.white,
+        title: Text("facebook", style: TextStyle(fontWeight:FontWeight.bold,fontSize: 25,color: Colors.blue[500])),
+        centerTitle: true,
+        leading: IconButton(
+          iconSize: 30,
+          icon: Icon(Icons.menu),
+          color: Colors.blue[500],
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            iconSize: 30,
+            icon: Icon(Icons.search),
+            color: Colors.blue[500],
+            onPressed: () {},
+          ),
+          IconButton(
+            iconSize: 30,
+            icon: Icon(Icons.message),
+            color: Colors.blue[500],
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
